6e9025d634900fa33462d5aba5f2e202c8f91a7c
hatschi <145645279+hatschi2023@users.noreply.github.com>
2024-10-24 23:25:43 +0900

    Update factorial.ts

--- kadai2/factorial.ts
+++ kadai2/factorial.ts
@@ -2,7 +2,7 @@
 const input = prompt("正の整数を入力：");
 const N = Number(input);
 
-// / 数値以外、０以下の整数、少数の入力は無効とする
+// 数値以外、０以下の整数、少数の入力は無効とする
 if (isNaN(N) || N <= 0 || !Number.isInteger(N)) {
     alert("有効な正の整数を半角数字で入力してください");
 }
