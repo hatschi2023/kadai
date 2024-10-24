beb3ddd8831e70d30a843f49e3b6454d67ccbb07
hatschi2023 <145645279+hatschi2023@users.noreply.github.com>
2024-10-24 22:07:04 +0900

    Update script.js

--- kadai1/script.js
+++ kadai1/script.js
@@ -1,17 +1,17 @@
 // クリック時にライトの色を指定の色に変更
-document.getElementById("red").addEventListener("click",function(){
+document.getElementById("light-red").addEventListener("click",function(){
     reset();
     this.classList.remove("gray");
     this.classList.add("red", "selected");
 });
 
-document.getElementById("yellow").addEventListener("click",function(){
+document.getElementById("light-yellow").addEventListener("click",function(){
     reset();
     this.classList.remove("gray");
     this.classList.add("yellow", "selected");
 });
 
-document.getElementById("blue").addEventListener("click",function(){
+document.getElementById("light-blue").addEventListener("click",function(){
     reset();
     this.classList.remove("gray");
     this.classList.add("blue", "selected");
@@ -19,7 +19,7 @@ document.getElementById("blue").addEventListener("click",function(){
 
 // ライトの色をグレーにリセット
 function reset(){
-    document.getElementById("red").className = "light gray";
-    document.getElementById("yellow").className = "light gray";
-    document.getElementById("blue").className = "light gray";
+    document.getElementById("light-red").className = "light gray";
+    document.getElementById("light-yellow").className = "light gray";
+    document.getElementById("light-blue").className = "light gray";
 }
